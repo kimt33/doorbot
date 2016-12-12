339c7d7aa681bb2fcc2219956aecd4e31fe167b8
Taewon D. Kim <kimt33@mcmaster.ca>
2016-12-12 02:38:23 -0500

    Make group meeting nice
    Documentations, consistency with other code, what not

--- bot/group_meeting.py
+++ bot/group_meeting.py
@@ -4,10 +4,10 @@ Takes commands from Slack client and translate them into script
 
 """
 import re
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 from random import random
-from .action import Action
-from .brain import BadInput, Messaging
+from .action import Action, BadInput, Messaging
+from .utils import nice_options, where_from_identifiers
 
 class GroupMeeting(Action):
     """ Action class for group meeting management
@@ -34,7 +34,7 @@ class GroupMeeting(Action):
         self.col_ids = {'id':0,
                         'date':1,
                         'presenter':2,
-                        'slack_chair':3,
+                        'chair':3,
                         'title':4,}
 
     @property
@@ -43,15 +43,27 @@ class GroupMeeting(Action):
 
     @property
     def init_response(self):
-        return ('I can only do one of {0}'
-                ' when managing group meetings').format(self.options.keys())
+        return ('I can only do one of {0} when managing group meetings'
+                ''.format(nice_options(self.options.keys(), 'or')))
 
     @property
     def options(self):
         return {'add' : self.add,
-                'recount' : self.recount}
+                'list' : self.list}
 
     def is_valid_date(self, date_str):
+        """ Checks if given date is valid
+
+        Parameters
+        ----------
+        date_str : str
+            Date written as yyyy-mm-dd
+
+        Returns
+        -------
+        True if valid
+        False if not
+        """
         if date_str in ['next']:
             return True
         try:
@@ -60,22 +72,10 @@ class GroupMeeting(Action):
         except ValueError:
             return False
 
-    def _find_from_identifiers(self, *identifiers):
-        where_command = ''
-        vals = []
-        if len(identifiers) > 0:
-            where_command += 'WHERE '
-            keys = identifiers[0::2]
-            vals = identifiers[1::2]
-            for key in keys:
-                where_command += '{0}=? '.format(key)
-
-        self.cursor.execute('SELECT * FROM group_meetings {0} '.format(where_command), vals)
-        rows = self.cursor.fetchall()
-        return where_command, vals, rows
-
 
     def select_member_volunteer(self, job='', date_str=''):
+        """ Asks users to volunteer
+        """
         pass
 
     def select_member_random(self, date_obj, job):
@@ -83,19 +83,16 @@ class GroupMeeting(Action):
 
         Parameters
         ----------
-        criteria : str
-            How each member is selected
-            One of ['random', 'volunteer', 'myself as']
+        date_obj : datetime.date
+            Date of presentation
         job : str
             What role is being selected
             One of ['presenter', 'chair]
-        date : str
-            Date of presentation
-            'next' or 'yyyy/mm/dd'
         """
         # find people that are present
         people_present = []
         for row in self.cursor.execute('SELECT id, name, dates_away FROM members'):
+            # translate date
             dates = re.findall(r'\d\d\d\d-\d\d-\d\d', row[2])[:2]
             dates = [datetime.strptime(i, '%Y-%m-%d').date() for i in dates]
             to_dates = dates[0::2]
@@ -149,33 +146,53 @@ class GroupMeeting(Action):
         return people_present[probs.index(max(probs))]
 
     def add(self, date_str='', job='', person=''):
+        """ Add a presentation
+
+        Parameters
+        ----------
+        date_str : str
+            String that describes the date
+            In form yyyy-mm-dd
+        job : str
+            One of 'presenter' or 'chair'
+        person : str
+            Some sort of identification of person
+            Can be one of 'id', 'userid', 'slack_id', 'name'
+        """
         if not self.is_valid_date(date_str):
             raise BadInput('What is the date of the presentaton?'
-                                       ' It must be one of "next" or "yyyy-mm-dd".')
+                           ' It must be one of "next" or "yyyy-mm-dd".')
 
         # get the data
-        self.cursor.execute('SELECT * FROM group_meetings ORDER BY date')
-        rows = self.cursor.fetchall()
+        self.cursor.execute('SELECT * FROM group_meetings ORDER BY date DESC')
+        row = self.cursor.fetchone()
 
         # find the date
         date_obj = None
         if date_str == 'next':
-            last_date = datetime.strptime(rows[-1][1], '%Y-%m-%d').weekday()
-            weekday_diff = datetime.today().weekday() - last_date
-            date_obj = datetime.today().date() + timedelta(7) + weekday_diff
+            last_date = datetime.strptime(row[1], '%Y-%m-%d').weekday()
+            weekday_diff = timedelta(days=7 - datetime.today().weekday() + last_date)
+            date_obj = datetime.today().date() + weekday_diff
             # FIXME: message
         else:
             date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
 
-        if job not in ['presenter', 'chair']:
+        # find job
+        if job not in ['presenter', 'chair', 'both']:
             raise BadInput('What job would you like me to assign?'
-                                       ' It must be one of "presenter" or "chair".',
-                                       args=(date_str,))
+                           ' It must be one of "presenter", "chair" or "both".',
+                           args=(date_str,))
+        # FIXME: garbage stuff
+        is_fucking_hassel = False
+        if job == 'both':
+            job = 'presenter'
+            is_fucking_hassel = True
 
+        #find person
         if person == '':
             raise BadInput('Who would you like to assign?'
-                                       ' It should be a name or "random"',
-                                       args=(date_str, job, ))
+                           ' It should be a name or "random"',
+                           args=(date_str, job, ))
 
         if person == 'random':
             person = self.select_member_random(date_obj, job)
@@ -185,20 +202,20 @@ class GroupMeeting(Action):
         matches = self.cursor.fetchall()
         if len(matches) == 0:
             raise BadInput('I could not find anyone whose id, name,'
-                                       ' userid, or slackid is {0}.\n'
-                                       'Would you care to try again?'.format(person),
-                                       args=(date_str, job,))
+                           ' userid, or slackid is {0}.\n'
+                           'Would you care to try again?'.format(person),
+                           args=(date_str, job,))
         elif len(matches) > 1:
             raise BadInput('I found more than one person whose id, name,'
-                                       ' userid, or slackid is {0}.\n'
-                                       'Would you care to try again?'.format(person),
-                                       args=(date_str, job,))
+                           ' userid, or slackid is {0}.\n'
+                           'Would you care to try again?'.format(person),
+                           args=(date_str, job,))
         else:
             person = matches[0][0]
 
         # Add data
         self.cursor.execute('SELECT * FROM group_meetings WHERE date=?',
-                            date_obj.isoformat())
+                            (date_obj.isoformat(), ))
         rows = self.cursor.fetchall()
         if len(rows) == 0:
             self.cursor.execute('INSERT INTO group_meetings (date, {0})'
@@ -214,32 +231,85 @@ class GroupMeeting(Action):
         else:
             # FIXME
             raise Messaging('There are more than one meetings to assign to.'
-                                   " I don't know what to do.")
+                            " I don't know what to do.")
         self.db_conn.commit()
 
-    def recount(self, *identifiers):
+        #FIXME: PLOP PLOP
+        if is_fucking_hassel:
+            self.add(date_str, 'chair', 'random')
+
+    def list(self):
         """ Shows all presentations that satifies some conditions
 
         Parameters
         ----------
-        identifiers : list
-            Identifier of the presentations
-            Each entry is string where ':' delimits the identifier from value
-            e.g. ['presenter:name', 'chair:name']
-
         """
-        for i in identifiers:
-            if i.split(':')[0] not in ['presenter', 'chair', 'title', 'date']:
-                raise BadInput('All given options are the identifiers of a particular presentation.'
-                                           ' Each identifier must be one of "presenter", "chair", "title", or "date".'
-                                           ' The value of each identifier is delimited by "=".'
-                                           ' For example, "presenter:person1 chair:person2"\n')
-        where_command, vals, row = self._find_from_identifiers(*identifiers)
-        if where_command == '' and row == 0:
-            raise BadInput('I could not find anyone using {0}'.format(identifiers))
-        self.cursor.execute('SELECT * FROM group_meetings ORDER BY date DESC {0}'
-                            ''.format(where_command))
+        #FIXME: identifier?
+        self.cursor.execute('SELECT * FROM group_meetings ORDER BY date DESC')
         rows = self.cursor.fetchall()
         message = '{0:<4}{1:<10}{2:<20}{3:<20}{4:<40}\n'.format('id', 'date', 'presenter', 'chair', 'title')
-        message += '\n'.join('{0:<4}{1:<10}{2:<20}{3:<20}{4:<40}'.format(*row) for row in rows)
+        for row in rows:
+            data_id = row[0]
+            date = row[1]
+            self.cursor.execute('SELECT name FROM members WHERE id=?', (row[2],))
+            presenter = str(self.cursor.fetchone())
+            self.cursor.execute('SELECT name FROM members WHERE id=?', (row[3],))
+            chair = str(self.cursor.fetchone())
+            title = row[4]
+            message += '{0:<4}{1:<10}{2:<20}{3:<20}{4:<40}\n'.format(data_id, date, presenter, chair, title)
         raise Messaging(message)
+
+    def modify(self, item='', to_val='', *identifiers):
+        """ Modifies existing group meeting data
+
+        Parameters
+        ----------
+        item : str
+            One of 'id', 'date', 'presenter', 'chair', or 'title'
+            or 'permission'
+        to_val : str
+            Value to change to
+        identifiers : list
+            List of alternating key and value of the person
+        """
+        if item == '' or item not in self.col_ids.keys():
+            raise BadInput('What would you like to change? It should be'
+                           ' one of {0}'.format(nice_options(self.col_ids.keys())))
+        if to_val == '':
+            raise BadInput('What would you like to change it to?',
+                           args=(item,))
+
+        if item == 'date' and not self.is_valid_date(to_val):
+            raise BadInput('The date must be one of "next" or "yyyy-mm-dd".')
+
+        # check identifier
+        if len(identifiers) == 0 or (len(identifiers) % 2 == 1 and
+                                     identifiers[-1] not in self.col_ids.keys()):
+            raise BadInput('What can you tell me about this presentation?'
+                           ' You can give me one of {0}.'
+                           ''.format(nice_options(self.col_ids.keys())),
+                           args=(item, to_val) + tuple(identifiers[:-1]))
+        elif len(identifiers) % 2 == 1:
+            raise BadInput('What is the {0} of this presentation?'
+                           ''.format(identifiers[-1]),
+                           args=(item, to_val) + tuple(identifiers))
+
+        where_command, vals = where_from_identifiers(*identifiers)
+        self.cursor.execute('SELECT * FROM members {0} '.format(where_command), vals)
+        rows = self.cursor.fetchall()
+        if len(rows) == 0:
+            messages = ['{0} is {1}'.format(i, j) for i, j in zip(identifiers, identifiers[1:])]
+            raise Messaging('I could not find any presentation where {0}'
+                            ''.format(nice_options(messages, 'and')))
+        elif len(rows) > 1:
+            messages = ['{0} is {1}'.format(i, j) for i, j in zip(identifiers, identifiers[1:])]
+            raise BadInput('There seems to be more than one presentation where {0}.'
+                           ' Could you tell me more about this person?'
+                           ' You can give me one of {1}.'
+                           ''.format(nice_options(messages, 'and'),
+                                     nice_options(self.col_ids.keys())),
+                           args=(item, to_val) + tuple(identifiers))
+        else:
+            self.cursor.execute('UPDATE members SET {0}=? WHERE id=?'.format(item),
+                                (to_val, rows[0][0]))
+            self.db_conn.commit()
